3bbdab76522e06acd1e5f2430fab54754944a480
Nikhil Kherajani <104251165+Nikhil-Kherajani@users.noreply.github.com>
2022-09-26 23:56:17 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -1,29 +1,35 @@
-from socket import timeout
 from plyer import notification
 import time
-import sys
 
-def notifyme(title , message):
+
+def notifyme(title, message):
     notification.notify(
-        title = title,
-        message = message,
-        app_icon = None,
-        timeout = 5,
+        title=title,
+        message=message,
+        app_icon=None,
+        timeout=5,
     )
-'''    
-while(True):
+
+
+nk = 0
+
+
+while (True):
+    print("upr wala ", nk)
     s = {}
     s[1] = "none"
-    f = open("d:\\nikhil python\\eye saver\\notification.txt","r")
-    s = f.readlines()   
-    print(int(s[0])) 
+    f = open("notification.txt", "r")
+    s = f.readlines()
+    print(int(s[0]))
     time.sleep(1)
-    if(int(s[0]) == 1):
+    if (int(s[0]) == 1):
         f.close()
-        notifyme("or bhai" , "break bhi le le")    
-        time.sleep(15)
-        if(int(s[0]) == 0):
-            sys.exit()'''
+        nk = 1
+        notifyme("EYE SAVER", "It's break time! Take a break of atleast 20 seconds")
+        time.sleep(1200)
+        print("ander wala ", nk)
 
-notifyme("Break bhi le" , "bhai yaa jaanu break le lo")    
-time.sleep(5)            
+    elif (int(s[0]) == 0):
+        if (nk == 1):
+            print("last", nk)
+            break
